3de81769765b2cc156f1919ba5d24f47120eb3cc
unknown <ismayilrha@code.edu.az>
2022-05-22 23:51:43 +0400

    practise

---
+++ Xasay_Class/Xasay_Class/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Xasay_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            user.Name = "xasay";
+
+            user.Surname = "Musayev";
+
+            user.Password = "111xasMusayev";
+
+            user.CheckPassword();
+        }
+    }
+}
---
+++ Xasay_Class/Xasay_Class/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xasay_Class
+{
+    class User
+    {
+        public string Name;
+        public string Surname;
+        public string Password;
+
+
+        public void CheckPassword()
+        {
+            bool status = true;
+            if(Password.Length < 6)
+            {
+                Console.WriteLine("Please enter correct password \nPassword must be over 6 charcter");
+                status = false;
+            }
+
+
+            if(status == true)
+            {
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i <= Password.Length - Name.Length; i++)
+                {
+                    for (int j = i; j < Name.Length + i; j++)
+                    {
+                        stringBuilder.Append(Password[j]);  //123xasay
+                    }
+
+                    if (stringBuilder.ToString().ToLower() == Name.ToLower())
+                    {
+                        Console.WriteLine("Password cannot contain part of name");
+                    }
+                   
+                    stringBuilder.Clear(); 
+
+                }
+
+
+
+
+                //for (int i = 0; i < Password.Length - Name.Length; i++)  // xasay123  xasay  //123xasay
+                //{
+                //    // test+=name  // xasay + musyev
+                //    stringBuilder.Append(Name);
+                //    if (stringBuilder.ToString().ToLower() == Password.ToLower())
+                //    {
+
+                //        Console.WriteLine("Password cannot contain part of name");
+                //        break;
+                //    }
+                //    stringBuilder.Clear();
+
+                //}
+
+
+            }
+            
+        }
+    }
+}
